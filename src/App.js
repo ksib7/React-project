10f451531d115ef687ab9a9e33fcefcc75cde0f9
ksib7 <sibirkin70@gmail.com>
2022-03-15 23:59:38 +0300

    Homework №2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import { Message, MessageEl } from "./Message";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
 
-const msg = {
-  text: "Text",
-};
-
 function App() {
+  let [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    if (messageList[messageList.length - 1] !== "Hello from bot") {
+      setTimeout(() => {
+        setMessageList([...messageList, "Hello from bot"]);
+      }, 700);
+    }
+  }, [messageList]);
+
+  let inputEl = document.querySelector("input");
+
+  const sendMessage = (message) => {
+    if (inputEl.value !== "") {
+      setMessageList([...messageList, message]);
+    }
+  };
+
   return (
-    <div className="App">
-      <Message text={msg.text} />
-      <MessageEl text={msg.text} />
+    <div className="container">
+      <form className="form" action="#">
+        <input
+          required
+          className="input"
+          type="text"
+          placeholder="Введите сообщение . . ."
+        ></input>
+        <button className="btn" onClick={() => sendMessage(inputEl.value)}>
+          Send message
+        </button>
+        <h1 className="message">
+          {messageList.map((item) => (
+            <div className="message__text">{item}</div>
+          ))}
+        </h1>
+      </form>
     </div>
   );
 }
